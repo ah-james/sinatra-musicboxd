b796f152fe0168ed198af2bceb6299a903adb799
ah-james <11ahj11@gmail.com>
2020-12-07 11:47:23 -0500

    posts new table

--- db/migrate/20201206191848_create_posts.rb
+++ db/migrate/20201206191848_create_posts.rb
@@ -1,7 +1,11 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-
+      t.string :album
+      t.string :artist
+      t.float :score
+      t.text :review
+      t.integer :user_id
       t.timestamps null: false
     end
   end
