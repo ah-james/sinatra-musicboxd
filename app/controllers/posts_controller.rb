83cfe11e9150163baf20252d7264f09ee1e22980
ah-james <11ahj11@gmail.com>
2020-12-09 17:17:26 -0500

    Post MVC

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
   get "/posts" do #index action
-    @user = User.find(session[:id])
+    # @user = User.find(session[:id])
     @posts = Post.all
     erb :"/posts/index"
   end
@@ -11,29 +11,32 @@ class PostsController < ApplicationController
   end
 
   post "/posts" do # create action
-    @post = Post.create(params)
+    @post = Post.create(:artist => params[:artist], :album => params[:album], :score => params[:score], :review => params[:review])
     redirect "/posts/#{@post.id}"
   end
 
   get "/posts/:id" do #show action
-    @post = Post.find_by_id(params)
+    @post = Post.find_by_id(params[:id])
     erb :"/posts/show"
   end
 
   get "/posts/:id/edit" do #edit action
-    @post = Post.find_by_id(params)
+    @post = Post.find_by_id(params[:id])
     erb :"/posts/edit"
   end
 
   patch "/posts/:id" do #update action
-    @post = Post.find_by_id(params)
-    
+    @post = Post.find_by_id(params[:id])
+    @post.album = params[:album]
+    @post.artist = params[:artist]
+    @post.score = params[:score]
+    @post.review = params[:review]
     @post.save
-    redirect "/posts/:id"
+    redirect "/posts/#{@post.id}"
   end
 
   delete "/posts/:id/delete" do #delete action
-    @post = Post.find_by_id(params)
+    @post = Post.find_by_id(params[:id])
     @post.delete
     redirect "/posts"
   end
